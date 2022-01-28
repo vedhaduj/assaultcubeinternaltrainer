5082763da5d882560c8e644de43cf7f35fa2b9f8
vedhaduj <97368023+vedhaduj@users.noreply.github.com>
2022-01-28 04:14:04 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <Windows.h>
+
+DWORD BaseAddress = 0x509B74;
+DWORD Entities = 0x50F500;
+
+int main()
+{
+
+    DWORD Health =  0xF8;
+    DWORD ARammo = 0x150;
+   //read
+	float healthval = *reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + Health);
+    int Arammoval = *reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + ARammo);
+    int players = *reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + Entities);
+
+//write
+	*reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + Health) = 99999;
+    *reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + ARammo) = 1337;
+
+}
+
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{    
+
+	if (dwReason == DLL_PROCESS_ATTACH) {
+
+		DisableThreadLibraryCalls(hModule);
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)main, NULL, NULL, NULL);
+
+	}
+	return TRUE;
+}
