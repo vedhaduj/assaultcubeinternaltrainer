a74d68d674da5ce117d67b9f380ebe0eeced0ee4
vedhaduj <97368023+vedhaduj@users.noreply.github.com>
2022-01-31 16:09:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ DWORD Entities = 0x50F500;
 
 int main()
 {
-
 	DWORD Health = 0xF8;
 	DWORD ARammo = 0x150;
 	//read
@@ -14,15 +13,11 @@ int main()
 	int Arammoval = *reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + ARammo);
 
 	//write
-	*reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + Health) = 99999;
-	*reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + ARammo) = 1337;
-
 	do
 	{
-		*reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + Health) = 99999;
-		*reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + ARammo) = 1337;
+	*reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + Health) = 99999;
+	*reinterpret_cast<int*>(*reinterpret_cast<uintptr_t*>(BaseAddress) + ARammo) = 1337;
 	} while (healthval <= 0 || healthval <= 100);
-
 }
 
 BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
