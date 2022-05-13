60c47143730452045a7f4d317ad275af067bbbc1
vedhaduj <97368023+vedhaduj@users.noreply.github.com>
2022-05-12 20:31:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,19 +12,26 @@ int main()
 	DWORD ARammo = 0x150;
 
 	int healthvalr = static_cast<int>((BaseAddress) + Health);
+	
 	int* healhval = &healthvalr;
+	
 	int BH = *(int*)BaseAddress + Health;
+	
+	//-------------------------------
 
 	int ammovaluer = static_cast<int>((BaseAddress)+ARammo);
+	
 	int* ammovalue = &ammovaluer;
+	
 	int AH = *(int*)BaseAddress + ARammo;
 	
     while(1)
 	{
-		*(int*)BH = 1337;
+		*(int*)BH = 1337; //derefcast to assign new values
 		*(int*)AH = 42069;	    
 	}
 }
+
 
 BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
